da40ba508e9d7b49fa5fc435aa35b40425e303c7
k-seo63 <xion0613@gmail.com>
2023-01-09 19:56:39 +0900

    Update: ディザリングの減色をパラメータ化

--- src/generator.py
+++ src/generator.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 
 # parameta
-img_name = "lena"
+img_name = "009"
 resize_ratio = 1
 saturation_ratio = 1.5
 
@@ -17,9 +17,9 @@ def main():
 
     # もろもろの処理
     resized_img = resize_img(input_img, resize_ratio)
-    corrected_img = saturation_up(gamma_curve(tone_curve(resized_img)), saturation_ratio)
-    dithered_64color_img = process_dithering_64color(corrected_img)
-    binarized_img = binarize_img(dithered_64color_img)
+    corrected_img = saturation_up(gamma_curve(resized_img), saturation_ratio)
+    dithered_img = process_dithering(corrected_img)
+    binarized_img = binarize_img(dithered_img)
 
     # 画像書き込み
     cv2.imwrite(str(output_img_path), binarized_img)
@@ -41,7 +41,11 @@ def process_dithering_8color(img):
     hight, width, _ = img.shape
 
     # 画像サイズのdither_matrixを取得(切り上げ演算)
-    tiled_dither_matrix_grayscale = np.tile(dither_matrix, (-(-hight // 4), -(-width // 4)))[:hight, :width]
+    tiled_dither_matrix_grayscale = np.tile(
+        dither_matrix, 
+        (-(-hight // 4), -(-width // 4))
+    )[:hight, :width]
+
     tiled_dither_matrix = np.stack(
         [tiled_dither_matrix_grayscale, tiled_dither_matrix_grayscale, tiled_dither_matrix_grayscale],
         axis=2
@@ -67,7 +71,11 @@ def process_dithering_64color(img):
     hight, width, _ = img.shape
 
     # 画像サイズのdither_matrixを取得(切り上げ演算)
-    tiled_dither_matrix_grayscale = np.tile(dither_matrix, (-(-hight // 4), -(-width // 4)))[:hight, :width]
+    tiled_dither_matrix_grayscale = np.tile(
+        dither_matrix, 
+        (-(-hight // 4), -(-width // 4))
+    )[:hight, :width]
+
     tiled_dither_matrix = np.stack(
         [tiled_dither_matrix_grayscale, tiled_dither_matrix_grayscale, tiled_dither_matrix_grayscale],
         axis=2
@@ -91,6 +99,46 @@ def process_dithering_256color(img):
     """
     # TODO: 実装
     return 
+
+
+def process_dithering(img, level=8):
+    """
+    ディザリングで R:G:B=level:level:level[bit] の level^3[色] へ減色
+    """
+    # 4x4配列ディザリングで参照するmatrix
+    dither_matrix = np.array([
+        [0, 8, 2, 10],
+        [12, 4, 14, 6],
+        [3, 11, 1, 9],
+        [15, 7, 13, 5]
+    ]) * (1/4**2) * 32 + ((256-32)/2) 
+    
+    # 画像サイズの取得
+    hight, width, _ = img.shape
+
+    # 画像サイズのdither_matrixを取得(切り上げ演算)
+    tiled_dither_matrix_grayscale = np.tile(
+        dither_matrix, 
+        (-(-hight // 4), -(-width // 4))
+    )[:hight, :width]
+
+    tiled_dither_matrix = np.stack(
+        [tiled_dither_matrix_grayscale, tiled_dither_matrix_grayscale, tiled_dither_matrix_grayscale],
+        axis=2
+    )
+
+    # 入力画像とMatrixの差を取り、それを0-255の値域におさめる
+    diff_img_matrix = (img - tiled_dither_matrix + 255) / 2
+
+    # うまいこと<level>値化する
+    for l in range(level):
+        processed_img = np.where(
+            (255*l/level <= diff_img_matrix) & (diff_img_matrix < 255*(l+1)/level), 
+            255*l // (level-1), 
+            diff_img_matrix
+        )
+
+    return processed_img.astype(np.uint8)
 
 
 def binarize_img(img):
